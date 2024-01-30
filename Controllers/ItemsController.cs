363bef65a830fdaca02a95f4dd1f18a2109bf661
drose433 <35655304+drose433@users.noreply.github.com>
2024-01-30 17:11:08 +0100

    Add Items Controller and Handle Errors

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemController : ControllerBase
+    {
+        private readonly InMemItemsRepository _repository;
+
+        public ItemController()
+        {
+            _repository = new InMemItemsRepository();
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = _repository.GetItems();
+            return items;
+        }
+
+        //GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItems(Guid id)
+        {
+            var item = _repository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
